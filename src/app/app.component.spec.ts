6142e69cbfe973b2f10a3208ee3d9250044f3762
Johnson <matthew.johnson@continis.ch>
2025-05-26 16:29:52 +0200

    add swipe buttons init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'swipe-cards' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('swipe-cards');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'swipe-cards';
+    cards = ['Card 1', 'Card 2', 'Card 3', 'Card 4'];
+    currentIndex = 0;
+
+    startX = 0;
+    currentX = 0;
+
+    startY = 0;
+    currentY = 0;
+
+    animationDirection: string | null = null;
+
+    get visibleCards() {
+        return this.cards.slice(this.currentIndex, this.currentIndex + 3);
+    }
+
+    onTouchStart(event: TouchEvent) {
+        this.startX = event.touches[0].clientX;
+        this.startY = event.touches[0].clientY;
+    }
+
+    onTouchMove(event: TouchEvent) {
+        this.currentX = event.touches[0].clientX - this.startX;
+        this.currentY = event.touches[0].clientY - this.startY;
+    }
+
+    onTouchEnd(event: TouchEvent) {
+        if (this.currentX > 120) {
+            this.swipe('right');
+        } else if (this.currentX < -120) {
+            this.swipe('left');
+        } else if (this.currentY < -120) {
+            this.swipe('up');
+        } else {
+            // reset position if no swipe
+            this.currentX = 0;
+            this.currentY = 0;
+        }
+    }
+
+    swipe(direction: string) {
+        this.animationDirection = direction;
+        setTimeout(() => {
+            this.currentIndex++;
+            if (this.currentIndex >= this.cards.length) {
+                this.currentIndex = 0;
+            }
+            this.animationDirection = null;
+            // reset drag coords so next card is reset
+            this.currentX = 0;
+            this.currentY = 0;
+        }, 300)
+    }
+}
+
+
+enum ESwipeDirection {
+    UP = "UP",
+    LEFT = "LEFT",
+    RIGHT = "RIGHT",
 }
