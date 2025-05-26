45b5bba80222e575884e596521aafa91ba46ec25
Johnson <matthew.johnson@continis.ch>
2025-05-26 16:56:40 +0200

    move swipe-cards to own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,87 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterOutlet],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    cards = ['Card 1', 'Card 2', 'Card 3', 'Card 4'];
-    currentIndex = 0;
 
-    coordinates: ICoordinates = {
-        startX: 0,
-        startY: 0,
-        currentX: 0,
-        currentY: 0,
-    }
-
-    animationDirection: string | null = null;
-
-    get visibleCards() {
-        return this.cards.slice(this.currentIndex, this.currentIndex + 3);
-    }
-
-    get currentX() {
-        return this.coordinates.currentX;
-    }
-
-    get currentY() {
-        return this.coordinates.currentY;
-    }
-
-    onTouchStart(event: TouchEvent) {
-        this.coordinates.startX = event.touches[0].clientX;
-        this.coordinates.startY = event.touches[0].clientY;
-    }
-
-    onTouchMove(event: TouchEvent) {
-        this.coordinates.currentX = event.touches[0].clientX - this.coordinates.startX;
-        this.coordinates.currentY = event.touches[0].clientY - this.coordinates.startY;
-    }
-
-    onTouchEnd(event: TouchEvent) {
-        if (this.coordinates.currentX > 120) {
-            this.swipe(ESwipeDirection.RIGHT);
-        } else if (this.coordinates.currentX < -120) {
-            this.swipe(ESwipeDirection.LEFT);
-        } else if (this.coordinates.currentY < -120) {
-            this.swipe(ESwipeDirection.UP);
-        } else {
-            // reset position if no swipe
-            this.coordinates.currentX = 0;
-            this.coordinates.currentY = 0;
-        }
-    }
-
-    swipe(direction: string) {
-        this.animationDirection = direction;
-        setTimeout(() => {
-            this.currentIndex++;
-            if (this.currentIndex >= this.cards.length) {
-                this.currentIndex = 0;
-            }
-            this.animationDirection = null;
-            // reset drag coords so next card is reset
-            this.coordinates.currentX = 0;
-            this.coordinates.currentY = 0;
-        }, 300)
-    }
-}
-
-
-enum ESwipeDirection {
-    UP = "up",
-    LEFT = "left",
-    RIGHT = "right",
-}
-
-interface ICoordinates {
-    startX: number,
-    startY: number,
-    currentX: number,
-    currentY: number
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'swipe-card',
+        loadComponent: () => import('./swipe-card/swipe-card.component').then(m => m.SwipeCardComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'swipe-card',
+        pathMatch: 'full'
+    }
+];
